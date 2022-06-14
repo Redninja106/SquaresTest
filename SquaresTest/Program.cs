ff30402f47b770cbd46ac8e0c16ebfaee7f13d5f
Ryan Andersen <45476006+Redninja106@users.noreply.github.com>
2022-06-14 19:12:10 -0400

    Add project files.

---
+++ SquaresTest/Program.cs
@@ -0,0 +1,5 @@
+﻿using SimulationFramework.Desktop;
+using SquaresTest;
+
+var app = new SquaresTestApp();
+app.RunDesktop();
---
+++ SquaresTest/SquaresTestApp.cs
@@ -0,0 +1,33 @@
+﻿using SimulationFramework;
+using SimulationFramework.Drawing.Canvas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquaresTest;
+
+internal class SquaresTestApp : Simulation
+{
+    List<Vector2> positions;
+
+    public override void OnInitialize(AppConfig config)
+    {
+        positions = new List<Vector2>();
+    }
+
+    public override void OnRender(ICanvas canvas)
+    {
+        if (Mouse.IsButtonPressed(MouseButton.Left))
+        {
+            positions.Add(Mouse.Position);
+        }
+
+        for (int i = 0; i < positions.Count; i++)
+        {
+            canvas.DrawRect(positions[i], new Vector2(50, 50));
+        }
+    }
+}
